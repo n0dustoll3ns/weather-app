646b6f2c171d1a99b9664f167802a36b8804095e
n0dustoll3ns <fominykh.dg@gmail.com>
2022-05-02 20:58:17 +0700

    add API res and loader interface

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,79 @@
 import React from "react";
 import WeatherBody from "./WeatherBody/WeatherBody";
+import './App.css';
+import weather from '../apis/api'
+import Loader from '.Loader/Loader'
+import SearchBar from '.SearchBar/SearchBar'
 
-const App = () => {
-  return (
-    <div className="App">
-      <div className="weatherContainer pt-3 pb-3">
-        <WeatherBody day={'Mon'} icon={'Sun'} minTemp={15} maxTemp={25} />
-        <WeatherBody day={'Tue'} icon={'Mistake'} minTemp={5} maxTemp={10} />
-        <WeatherBody day={'Wed'} icon={'Snowing'} minTemp={0} maxTemp={-5} />
-        <WeatherBody day={'Thu'} icon={'Rain'} minTemp={12} maxTemp={14} />
-        <WeatherBody day={'Fri'} icon={'Snowing'} minTemp={-10} maxTemp={-22} />
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      temp: [],
+      city: null,
+      isLoaded: false
+    }
+  }
+
+  componentDidMount() {
+    this.setState({ isLoaded: true });
+  }
+
+  searchCity = async city => {
+    await weather
+      .get(`daily?city=${city}&key=2e9ad84276ce48268c51707622214532`)
+      .then(res => {
+        const temp = res.data.data;
+        const city = res.data.city_name;
+
+        this.setState({
+          temp, city, isLoaded: true
+        });
+      });
+  };
+  render() {
+
+    const minTemp = this.state.temp.map(el => {
+      return parseInt(el.low_temp)
+    })
+
+    const maxTemp = this.state.temp.map(el => {
+      return parseInt(el.max_temp)
+    })
+
+    const icon = this.state.temp.map(el => {
+      return el.weather.code
+    })
+
+    const description = this.state.temp.map(el => {
+      return el.weather.description;
+    })
+
+    if (!this.state.isLoaded) {
+      return <Loader msg={'Loading...'} />
+    }
+
+
+    
+
+    return (
+      <div className="App" >
+        <div className="weatherContainer pt-3 pb-3">
+          <WeatherBody day={'Mon'} icon={'Sun'} minTemp={15} maxTemp={25} />
+          <WeatherBody day={'Tue'} icon={'Mistake'} minTemp={5} maxTemp={10} />
+          <WeatherBody day={'Wed'} icon={'Snowing'} minTemp={0} maxTemp={-5} />
+          <WeatherBody day={'Thu'} icon={'Rain'} minTemp={12} maxTemp={14} />
+          <WeatherBody day={'Fri'} icon={'Snowing'} minTemp={-10} maxTemp={-22} />
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
+
+
+
+
 
 export default App;
