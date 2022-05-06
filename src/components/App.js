ff80b5f96989118def01c06bd282d43294052112
n0dustoll3ns <fominykh.dg@gmail.com>
2022-05-06 21:43:07 +0700

    add section with content to main block

--- src/components/App.js
+++ src/components/App.js
@@ -1,74 +1,62 @@
 import React from "react";
-import WeatherBody from "./WeatherBody/WeatherBody";
 import './App.css';
 import weather from '../apis/api';
 import Loader from "./Loader/Loader";
-import SearchBar from './SearchBar/SearchBar';
 import Header from "./Header/Header";
 import Summary from "./Summary/Summary";
+import Detail from "./Detail/Detail";
+
 class App extends React.Component {
   constructor(props) {
     super(props);
-
     this.state = {
       temp: [],
       city: null,
       isLoaded: false
     }
   }
-
   componentDidMount() {
     this.setState({ isLoaded: true });
   }
-
   searchCity = async city => {
     await weather
       .get(`daily?city=${city}&key=2e9ad84276ce48268c51707622214532`)
       .then(res => {
         const temp = res.data.data;
         const city = res.data.city_name;
-
         this.setState({
           temp, city, isLoaded: true
         });
       });
   };
+
   render() {
+    // const minTemp = this.state.temp.map(el => {
+    //   return parseInt(el.low_temp)
+    // })
 
-    const minTemp = this.state.temp.map(el => {
-      return parseInt(el.low_temp)
-    })
+    // const maxTemp = this.state.temp.map(el => {
+    //   return parseInt(el.max_temp)
+    // })
 
-    const maxTemp = this.state.temp.map(el => {
-      return parseInt(el.max_temp)
-    })
+    // const icon = this.state.temp.map(el => {
+    //   return el.weather.code
+    // })
 
-    const icon = this.state.temp.map(el => {
-      return el.weather.code
-    })
-
-    const description = this.state.temp.map(el => {
-      return el.weather.description;
-    })
+    // const description = this.state.temp.map(el => {
+    //   return el.weather.description;
+    // })
 
     if (!this.state.isLoaded) {
       return <Loader msg={'Loading...'} />
     }
 
-
-
     return (
       <div className="App" >
         <Header></Header>
         <main>
-        <Summary></Summary>
-          <div className="detail">
-            <div className="today">
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente temporibus, aut quod deleniti ipsum dicta excepturi officia, molestias corporis voluptatibus ipsam doloremque ut, mollitia obcaecati odio ipsa et velit fuga? Lorem ipsum dolor sit amet consectetur adipisicing elit. A optio commodi quae adipisci? Quisquam quasi non vitae et exercitationem sequi eum ea. Explicabo dolore necessitatibus nemo dolorem et non repudiandae!</div>
-            <div className="map">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Mollitia reprehenderit quis, minus enim non ipsa earum, porro quisquam fugit fuga quasi consectetur voluptas. Facere veritatis reiciendis voluptate officiis quis? Fugiat? Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum tempora, aliquid harum minus dignissimos itaque incidunt, dolores eveniet explicabo sunt facilis, amet temporibus. Facilis enim earum magnam perspiciatis saepe eveniet!
-            </div>
-          </div>
+          <Summary></Summary>
+          <Detail></Detail>
           <div className="ten-days">
             Very very long Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos voluptatum quos placeat accusantium assumenda soluta, mollitia quaerat hic eum quia! Est sint dicta similique suscipit nobis quidem, ducimus fugit tempore?
           </div>
---
+++ src/components/Detail/Detail.js
@@ -0,0 +1,34 @@
+import React from "react";
+import Snow from '../../assets/icons/Snow-Col.svg'
+import './Detail.css'
+
+const Detail = () => {
+  return (
+    <div className="detail">
+      <div className="today">
+        <section className="title">
+          Village Baskerville <button type="submit" className="location_button">My Location</button>
+        </section>
+        <section className="time-and-yesterday-data">
+          Now is 18:98. This time yesterday +10°
+        </section>
+        <section className="time-and-yesterday-data">
+          <div className="temp">+16°</div>
+          <div className="temp-icon">
+            <img src={Snow} alt="sun" className="icon" />
+          </div>
+          <div className="temp">Partly Cloud</div>
+          <div className="feels-like">Feels like +12°</div>
+
+        </section>
+
+      </div>
+      <div className="map">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Mollitia reprehenderit quis, minus enim non ipsa earum, porro quisquam fugit fuga quasi consectetur voluptas. Facere veritatis reiciendis voluptate officiis quis? Fugiat? Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum tempora, aliquid harum minus dignissimos itaque incidunt, dolores eveniet explicabo sunt facilis, amet temporibus. Facilis enim earum magnam perspiciatis saepe eveniet!
+      </div>
+    </div>
+
+  );
+}
+
+export default Detail;
