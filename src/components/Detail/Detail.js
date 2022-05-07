9a701a21c1bca0ca68be9fae3d23137b9b32ff53
n0dustoll3ns <fominykh.dg@gmail.com>
2022-05-07 22:02:00 +0700

    add template of swiper to main block

--- src/components/Detail/Detail.js
+++ src/components/Detail/Detail.js
@@ -1,5 +1,7 @@
 import React from "react";
-import Snow from '../../assets/icons/Snow-Col.svg'
+import Snow from '../../assets/icons/Snow-Nocol.svg'
+import Sun from '../../assets/icons/Sun-Nocol.svg'
+import RainNight from '../../assets/icons/RainNight-Nocol.svg'
 import './Detail.css'
 
 const Detail = () => {
@@ -10,7 +12,7 @@ const Detail = () => {
           Village Baskerville <button type="submit" className="location_button">My Location</button>
         </section>
         <section className="time-and-yesterday-data">
-          Now is 18:98. This time yesterday +10°
+          Now is 18:48. This time yesterday +10°
         </section>
         <section className="time-and-yesterday-data">
           <div className="temp">+16°</div>
@@ -20,7 +22,128 @@ const Detail = () => {
             <div className="feels-like">Feels like +12°</div>
           </div>
         </section>
-
+        <section className="properties">
+          <div className="wind">
+            Wind is Blowing
+          </div>
+          <div className="humidity">25%</div>
+          <div className="pressure">725mmHg</div>
+          <div className="water-temp">0°</div>
+        </section>
+        <section className="hourly">
+          <div className="swiper-btn prev">⏴</div>
+          <ul className="swiper-wrapper">
+          <li className="hourly">
+              <div className="hour-time">
+                20:00
+              </div>
+              <img src={Sun} alt="" className="hour-icon" />
+              <div className="hour-temp">
+                +9°
+              </div>
+            </li>
+            <li className="hourly">
+              <div className="hour-time">
+                20:00
+              </div>
+              <img src={Sun} alt="" className="hour-icon" />
+              <div className="hour-temp">
+                +9°
+              </div>
+            </li>
+            <li className="hourly">
+              <div className="hour-time">
+                20:00
+              </div>
+              <img src={Sun} alt="" className="hour-icon" />
+              <div className="hour-temp">
+                +9°
+              </div>
+            </li>
+            <li className="hourly">
+              <div className="hour-time">
+                20:00
+              </div>
+              <img src={Sun} alt="" className="hour-icon" />
+              <div className="hour-temp">
+                +9°
+              </div>
+            </li>
+            <li className="hourly">
+              <div className="hour-time">
+                20:00
+              </div>
+              <img src={Sun} alt="" className="hour-icon" />
+              <div className="hour-temp">
+                +9°
+              </div>
+            </li>
+            <li className="hourly">
+              <div className="hour-time">
+                20:00
+              </div>
+              <img src={Sun} alt="" className="hour-icon" />
+              <div className="hour-temp">
+                +9°
+              </div>
+            </li>
+            <li className="hourly">
+              <div className="hour-time">
+                20:00
+              </div>
+              <img src={Sun} alt="" className="hour-icon" />
+              <div className="hour-temp">
+                +9°
+              </div>
+            </li>
+            <li className="hourly">
+              <div className="hour-time">
+                20:00
+              </div>
+              <img src={Sun} alt="" className="hour-icon" />
+              <div className="hour-temp">
+                +9°
+              </div>
+            </li>
+            <li className="hourly">
+              <div className="hour-time">
+                20:00
+              </div>
+              <img src={Sun} alt="" className="hour-icon" />
+              <div className="hour-temp">
+                +9°
+              </div>
+            </li>
+            <li className="hourly">
+              <div className="hour-time">
+                20:00
+              </div>
+              <img src={Sun} alt="" className="hour-icon" />
+              <div className="hour-temp">
+                +9°
+              </div>
+            </li>
+            <li className="hourly">
+              <div className="hour-time">
+                20:00
+              </div>
+              <img src={Sun} alt="" className="hour-icon" />
+              <div className="hour-temp">
+                +9°
+              </div>
+            </li>
+            <li className="hourly">
+              <div className="hour-time">
+                20:00
+              </div>
+              <img src={Sun} alt="" className="hour-icon" />
+              <div className="hour-temp">
+                +9°
+              </div>
+            </li>
+          </ul>
+          <div className="swiper-btn next">⏵</div>
+        </section>
       </div>
       <div className="map">
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Mollitia reprehenderit quis, minus enim non ipsa earum, porro quisquam fugit fuga quasi consectetur voluptas. Facere veritatis reiciendis voluptate officiis quis? Fugiat? Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum tempora, aliquid harum minus dignissimos itaque incidunt, dolores eveniet explicabo sunt facilis, amet temporibus. Facilis enim earum magnam perspiciatis saepe eveniet!
