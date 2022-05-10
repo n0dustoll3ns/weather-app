e8be1e295aacb1f055d84941c5c551e8b33fc290
n0dustoll3ns <fominykh.dg@gmail.com>
2022-05-10 16:51:56 +0700

    add react-selet package

--- src/shared/Header/Header.tsx
+++ src/shared/Header/Header.tsx
@@ -1,15 +1,32 @@
 import React from "react";
+import Select from 'react-select'
+
 import { GlobalSvgSelector } from "../../assets/icons/global/GlobalSvgSelector";
 import s from './Header.module.scss'
 
-interface Props {}
+interface Props { }
 
-export const Header = (prop:Props)=>{
+export const Header = (prop: Props) => {
+  const options = [
+    { value: 'chocolate', label: 'Chocolate' },
+    { value: 'strawberry', label: 'Strawberry' },
+    { value: 'vanilla', label: 'Vanilla' }
+  ]
+
+
   return <header className={s.header}>
     <div className={s.wrapper}>
-      <div className={s.logo}><GlobalSvgSelector id="header-logo"/></div>
+      <div className={s.logo}>
+        <GlobalSvgSelector id="header-logo" />
+      </div>
       <div className={s.title}>React Weather</div>
     </div>
-    <div className={s.wrapper}></div>
+    <div className={s.wrapper}>
+      <div className={s.change_theme}>
+        <GlobalSvgSelector id="change-theme" />
+          <Select options={options} />
+
+      </div>
+    </div>
   </header>
 }
