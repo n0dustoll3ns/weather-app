810f89b638e39585b52db0e927c4a8397784f8a8
n0dustoll3ns <fominykh.dg@gmail.com>
2022-05-10 15:10:36 +0700

    add routes and header

---
+++ src/shared/Header/Header.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import s from './Header.module.scss'
+
+interface Props {}
+
+export const Header = (prop:Props)=>{
+  return <header className={s.header}>Headeasdr</header>
+}
