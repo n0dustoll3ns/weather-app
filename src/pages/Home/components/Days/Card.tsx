b7fc821fd0912b497f2a91731441a731f4fdff37
n0dustoll3ns <fominykh.dg@gmail.com>
2022-05-11 17:14:52 +0700

    add Card component to element Days

--- src/pages/Home/components/Days/Card.tsx
+++ src/pages/Home/components/Days/Card.tsx
@@ -6,12 +6,21 @@ import { Day } from "./Days";
 import s from './Days.module.scss'
 
 interface Props {
-  day:Day;
-  
- }
+  day: Day;
 
-export const Card = (prop: Props) => {
-  return <div className={s.card}>
-
-  </div>
 }
+
+export const Card = ({ day }: Props) => {
+  return (
+    <div className={s.card}>
+      <div className={s.day}>{day.day}</div>
+      <div className={s.day__info}>{day.day_info}</div>
+      <div className={s.img}>
+        <GlobalSvgSelector id={day.icon_id} />
+      </div>
+      <div className={s.temp__day}>{day.temp_day}</div>
+      <div className={s.temp__night}>{day.temp_day}</div>
+      <div className={s.info}>{day.info}</div>
+    </div>
+  );
+};
