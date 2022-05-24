eb6dedb7ecf5fafc15b32efc876d310c7431913c
n0dustoll3ns <fominykh.dg@gmail.com>
2022-05-24 23:20:13 +0700

    fix backgorund css variable

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import { Route, Routes } from "react-router";
 import { Home } from './pages/Home/Home';
 import { MonthStatistics } from './pages/MonthStatistics/MonthStatistics';
 import { Header } from './shared/Header/Header';
+import { Popup } from './shared/Popup/Popup';
 
 function App() {
   return (
     <div className="container">
+      <Popup/>
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
