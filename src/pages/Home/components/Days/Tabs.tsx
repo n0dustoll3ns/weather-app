54f51db990b75ce8f189fb0b48ea3f3e22b70ed7
n0dustoll3ns <fominykh.dg@gmail.com>
2022-05-26 00:45:38 +0700

    add css variables for dark theme

--- src/pages/Home/components/Days/Tabs.tsx
+++ src/pages/Home/components/Days/Tabs.tsx
@@ -1,6 +1,8 @@
+import React from 'react';
+
 import s from './Days.module.scss';
 
-interface Props {}
+interface Props { }
 
 export const Tabs = (props: Props) => {
   const tabs = [
@@ -18,7 +20,7 @@ export const Tabs = (props: Props) => {
     <div className={s.tabs}>
       <div className={s.tabs__wrapper}>
         {tabs.map(tab => (
-          <div className={s.tab} key={tab.value}>
+          <div className={s.tab + ' ' + s.active} key={tab.value}>
             {tab.value}
           </div>
         ))}
